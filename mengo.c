2998b8a0f6a233d58d91c1c82769b22d35d77108
manoelargc <manoresende@outlook.com>
2022-05-20 17:02:16 -0300

    Create mengo.c
    escudo do flamengo com ascii

---
+++ mengo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC\n");
+    printf("CC   CC           CCCCCCCCCCCCCCCCCCC\n");
+    printf("CC  CCCCCCCCC     CCCCCCCCCCCCCCCCCCC\n");
+    printf("CC  C  CC CCCCC   C                CC\n");
+    printf("CC CC  CC C CCC   C                CC\n");
+    printf("CC CC  CC CCC     C                CC \nCC  CCCCCCCCC     CCCCCCCCCCCCCCCCCCC\nCC   CC CC CCC    CCCCCCCCCCCCCCCCCCC\nCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC\nCC                                 CC\nCC                                 CC\nCC                                 CC\n CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC\n CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC\n  CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC\n  CC                              CC\n   CC                            CC\n    CC                          CC \n     CCCCCCCCCCCCCCCCCCCCCCCCCCCC\n      CCCCCCCCCCCCCCCCCCCCCCCCC\n       CCCCCCCCCCCCCCCCCCCCCCC\n         CC               CC\n          CC            CC\n            CCC       CCC\n             CCC   CCC\n                CCC\n");
+}
