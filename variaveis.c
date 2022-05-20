2fb836a9f401eb231f2aaa1a3f12d22e1b4c4827
manoelargc <manoresende@outlook.com>
2022-05-20 17:23:10 -0300

    Create variaveis.c
    testando o que deu na aula

---
+++ variaveis.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	int idade=10, numPessoas=25, dist_cm=125;
+	float altura_metros= 1.25;
+	float totalAltura;
+	totalAltura= numPessoas*altura_metros;
+printf("total altura=%f de %d pessoas \n", totalAltura, numPessoas);
+}
