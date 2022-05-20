8981e6a42090444cc0bad98213a96967d09f399f
manoelargc <manoresende@outlook.com>
2022-05-20 17:13:20 -0300

    Create area da circunferencia.c

---
+++ area da circunferencia.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+    int main(){
+
+    float pi=3.14;
+    float raio=2.5;
+    float area;
+    area= pi*raio*raio;
+
+
+    printf("Ola! Sou uma calculadora de areas da circunferencia :) \n A formula eh dada por exemplo: pi*raio^2 \n A= %.2f*%.2f^2 \n neste exemplo o raio vale 2,5, entao: \nA= %.2f \n", pi, raio, area);
+
